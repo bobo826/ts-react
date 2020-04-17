8fe0df7ef92175478e17afd999527233fb91ed6c
bobo826 <2696468332@qq.com>
2020-04-17 13:54:51 +0800

    第一次提交代码

---
+++ src/components/Hello.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { useState } from 'react';
+
+function Hello() {
+  const [name, setName] = useState('Mary')
+  const [surname, setSurname] = useState('lili')
+  return (
+    <div>
+      <p>{ name } + { surname }</p>
+      <button onClick={() => setName('1')}>
+        set name
+      </button>
+      <button onClick={() => setSurname('2')}>
+        set surname 
+      </button>
+    </div>
+  );
+}
+
+export default Hello
